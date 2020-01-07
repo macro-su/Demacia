94d739f1cea15facc9bc6f82b2a32fce13464459
hong.su <hong.su@qiaofang.cn>
2020-01-07 19:53:47 +0800

    添加服务生产者

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 ### springcloud技能点
-- eureka 注册中心
+
+#### eureka 服务治理组件，包含服务注册与发现
+> eureka是阿基米德洗澡时发现浮力原理时发出的惊叹声，在微服务中寓意发现
+#### Hystrix 容错管理组件，实现了熔断器
+#### Ribbon 客户端负载均衡的服务调用组件
+#### Feign 基于Ribbon和Hystrix的声明式服务调用组件
+#### Zuul 网关组件，提供智能路由、访问过滤等功能
 
 > hosts 
 127.0.0.1 server1
---
+++ provider/src/main/java/com/macro/App.java
@@ -0,0 +1,13 @@
+package com.macro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
