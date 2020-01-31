fb9c22851d2c3af58fc59b580001f2e1886841fc
mrsuhong@163.com <suhong>
2020-01-31 18:11:59 +0800

    改造成docker 镜像模式

--- config/src/main/java/com/macro/App.java
+++ config/src/main/java/com/macro/App.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class Config {
+public class App {
     public static void main(String[] args) {
-        SpringApplication.run(Config.class, args);
+        SpringApplication.run(App.class, args);
     }
 }
--- provider/src/main/java/com/macro/App.java
+++ provider/src/main/java/com/macro/App.java
@@ -2,13 +2,12 @@ package com.macro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
-//创建服务注册中心
-@EnableEurekaServer
-public class Eureka {
+public class App {
     public static void main(String[] args) {
-        SpringApplication.run(Eureka.class, args);
+        SpringApplication.run(App.class, args);
     }
 }
